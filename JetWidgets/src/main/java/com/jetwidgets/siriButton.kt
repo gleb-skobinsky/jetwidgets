c3dba243ea59a3637da5906a38c0e8a88062e4e1
gleb-skobinsky <glebgytnik@gmail.com>
2023-06-06 20:40:12 +0300

    Rotating red square

--- JetWidgets/src/main/java/com/jetwidgets/siriButton.kt
+++ JetWidgets/src/main/java/com/jetwidgets/siriButton.kt
@@ -1,5 +1,6 @@
 package com.jetwidgets
 
+import android.graphics.Camera
 import androidx.compose.animation.core.withInfiniteAnimationFrameMillis
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
@@ -13,18 +14,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.center
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.unit.dp
 import java.io.Serializable
 import kotlin.math.cos
+import kotlin.math.round
 import kotlin.math.sin
 
 const val RADIUS_DP = 256f
 private const val TWO_PI = Math.PI.toFloat() * 2
 private const val DEFAULT_PHI = Math.PI.toFloat() / 1.7f
-private const val FIELD_OF_VIEW_FACTOR = .7f
+private const val FIELD_OF_VIEW_FACTOR = 1.0f
+private const val STROKE_TRANSFORMATION_OFFSET = 35f //considering phi value of π/1.7
 
 @Composable
 fun BoxScope.SiriButton() {
@@ -50,13 +57,27 @@ fun BoxScope.SiriButton() {
                 radius = 10.dp.toPx(),
                 center = Offset(size.width / 2, size.height / 2)
             )
+            val rectSize = 40.dp.toPx()
             val pointIn2D = map2Dto3D(point.phi, point.theta, time, RADIUS_DP.dp.toPx())
             scale(pointIn2D.projectedScale) {
-                drawCircle(
-                    color = Color.Red,
-                    radius = 10.dp.toPx(),
-                    center = Offset(pointIn2D.projectedX, pointIn2D.projectedY)
-                )
+                val matrix = android.graphics.Matrix()
+                val camera = Camera()
+                val x = pointIn2D.projectedX
+                val y = pointIn2D.projectedY + round(size.minDimension / 100)
+                drawIntoCanvas { canvas ->
+                    applyStrokeTransformations(pointIn2D.projectedX, size, camera)
+                    camera.getMatrix(matrix)
+                    matrix.preTranslate(-x, -y)
+                    matrix.postTranslate(x, y)
+                    canvas.save()
+                    canvas.nativeCanvas.concat(matrix)
+                    drawRect(
+                        color = Color.Red,
+                        size = Size(rectSize, rectSize),
+                        topLeft = Offset(pointIn2D.projectedX - rectSize / 2, pointIn2D.projectedY - rectSize / 2)
+                    )
+                    canvas.restore()
+                }
             }
         }
     }
@@ -103,3 +124,24 @@ data class RotatingPoint(
     val phi: Float,
     val theta: Float,
 )
+
+private fun applyStrokeTransformations(projectedX: Float, canvasDim: Size, camera: Camera) {
+    val canvasCenterX = canvasDim.center.x
+    val canvasStartX = canvasCenterX - canvasDim.minDimension / 2
+    val canvasEndX = canvasCenterX + canvasDim.minDimension / 2
+    val canvasStartOffsetX = canvasStartX + STROKE_TRANSFORMATION_OFFSET
+    val canvasEndOffsetX = canvasEndX - STROKE_TRANSFORMATION_OFFSET
+    if (projectedX < canvasCenterX) {
+        camera.rotate(
+            0f,
+            normalize(projectedX, canvasStartX, canvasCenterX, 80f, 0f),
+            normalize(projectedX, canvasStartX, canvasStartOffsetX, -35f, 0f)
+        )
+    } else {
+        camera.rotate(
+            0f,
+            normalize(projectedX, canvasCenterX, canvasEndX, 0f, -80f),
+            normalize(projectedX, canvasEndOffsetX, canvasEndX, 0f, 35f)
+        )
+    }
+}
