3867e0b1795bd9f94a0a4ff8354db474333e1dfa
gleb-skobinsky <glebgytnik@gmail.com>
2023-06-06 11:02:21 +0300

    Start working on siri

--- JetWidgets/src/main/java/com/jetwidgets/app.kt
+++ JetWidgets/src/main/java/com/jetwidgets/app.kt
@@ -2,14 +2,12 @@ package com.jetwidgets
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
